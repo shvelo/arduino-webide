c991785e74c94f7b60bbc1706efacd0e6c462b97
Nick Shvelidze <shveloo@gmail.com>
2013-06-14 22:36:16 +0400

    Color themes and stuff

--- web/script/app.js
+++ web/script/app.js
@@ -1,13 +1,30 @@
 $(function() {
 	var codeEditor;
 
+	var theme = localStorage.getItem("theme");
+	if(theme == null) {
+		theme = "solarized dark";
+		localStorage.setItem("theme", theme);
+	}
+
 	$.get("misc/default_code.txt", function(data) {
-		codeEditor = CodeMirror($("#code")[0], {
+		codeEditor = CodeMirror($("#code > div")[0], {
 		  value: data,
 		  mode:  "clike",
-		  theme: "solarized",
+		  theme: theme,
 		  indentWithTabs: true,
-		  indentUnit: 4
+		  indentUnit: 4,
+		  lineNumbers: true
 		});
+	});
+
+	var setTheme = function(selectedTheme) {
+		theme = selectedTheme;
+		localStorage.setItem("theme", selectedTheme);
+		codeEditor.setOption("theme", theme);
+	}
+
+	$("#theme-selector a").click(function() {
+		setTheme($(this).data("theme"));
 	});
 });
