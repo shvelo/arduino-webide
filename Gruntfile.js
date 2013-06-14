05615ce76334424e41e317d1a03325db0fb059b2
Nick Shvelidze <shveloo@gmail.com>
2013-06-14 15:14:38 +0400

    Add preview

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,12 +23,37 @@ module.exports = function(grunt) {
           "web/index.html": "web/index.html.jade"
         }
       }
+    },
+    watch: {
+      files: ['web/index.html.jade', 'web/style/app.less'],
+      tasks: ['default']
+    },
+    parallel: {
+      preview: {
+        tasks: [
+          {
+            grunt: true,
+            args: "server"
+          }, {
+            grunt: true,
+            args: "watch"
+          }
+        ]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-jade');
-
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-parallel');
 
   grunt.registerTask('default', ['less', 'jade']);
+
+  grunt.registerTask('server', "Run a server", function() {
+    this.async();
+    require('./server.js');
+  });
+
+  grunt.registerTask('preview', ['default', 'parallel:preview']);
 };
