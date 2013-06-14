a61e95ecd142bd4bbf5a59e9076f08b914a3dc31
Nick Shvelidze <shveloo@gmail.com>
2013-06-14 23:08:25 +0400

    Add Serial

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var io = require('socket.io').listen(80);
+var SerialPort = require("serialport").SerialPort;
+var serial;
+
+io.sockets.on('connection', function (socket) {
+	socket.on('serial-connect', function (data) {
+		serial = new SerialPort(data.port, {
+			baudrate: data.rate
+		});
+		serial.on("open", function () {
+			serialPort.on('data', function(data) {
+				socket.emit('serial-output', { data : data });
+			});
+			socket.emit('serial-connected', {});
+		});
+	});
+	socket.on('serial-input', function (data) {
+		if(serial) serial.write(data.data, function(err, results) {
+			socket.emit('serial-result', { err: err, results: results})
+		});
+	});
+});
 
 app.use(express.static(path.join(__dirname, 'web')));
 
