9fd8e081b966b2da1c731e55108fdc34cbd3aa50
Nick Shvelidze <shveloo@gmail.com>
2013-06-14 21:04:26 +0400

    Hide stuff in Windows script

--- arduino-webide.bat
+++ arduino-webide.bat
@@ -1,5 +1,7 @@
 
-if NOT EXIST %~dp0\node_modules call npm install %~dp0
-call grunt --version
-IF NOT %ERRORLEVEL% EQU 0 call npm install -g grunt-cli
-grunt --gruntfile %~dp0\Gruntfile.js chrome
+@ECHO OFF
+Echo First run may take long!
+if NOT EXIST %~dp0\node_modules call npm install %~dp0 >nul
+call grunt --version >nul
+IF NOT %ERRORLEVEL% EQU 0 call npm install -g grunt-cli >nul
+grunt --gruntfile %~dp0\Gruntfile.js chrome >nul
